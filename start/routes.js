5f831ac4e4f38ea2258413657e84173058ec06ec
AdhamWynston <adhamwynston@outlook.com>
2018-08-08 01:56:52 -0300

    Update routes

--- start/routes.js
+++ start/routes.js
@@ -5,48 +5,45 @@ const Route = use('Route')
 Route
 .group(() => {
 
-  // CATEGORY
-  Route
-  .resource('categories', 'Admin/CategoryController')
-  .apiOnly()
-  .middleware(['is:master'])
-
-// PRODUCT
+  // SELLER
   Route
   .resource('products', 'Seller/ProductController')
   .apiOnly()
   .middleware(['is:(seller || master)'])
 
-  // MENU OPTION
+  Route
+  .post('sellers', 'Seller/SellerController.store').middleware(['is:(seller || master)'])
+
+  Route
+  .put('sellers/:id', 'Seller/SellerController.update')
+
+  Route
+  .get('sellers/:id', 'Seller/SellerController.show')
+
+  Route
+  .resource('seller/users', 'Seller/UserController')
+  .apiOnly()
+  .middleware(['is:(seller || master)'])
+
   Route
   .resource('menu-options', 'Seller/MenuOptionController')
   .apiOnly()
   .middleware(['is:(seller || master)'])
 
 
-  // SELLER
-  Route
-  .post('sellers', 'Seller/SellerController.store').middleware(['is:(seller || master)'])
 
-  Route
-  .put('sellers/:id', 'SellerController.update')
-
-  Route
-  .put('sellers/:id', 'SellerController.show')
-
-  // USER SELLER
-  Route
-  .resource('seller/users', 'Seller/UserController')
-  .apiOnly()
-  .middleware(['is:(seller || master)'])
-
-  // USER CUSTOMER
+  //  CUSTOMER
     Route
     .get('users/:id', 'UserController.show')
     Route
     .put('users/:id', 'UserController.update')
 
-    // ACCOUNT
+    // ADMIN
+    Route
+  .resource('categories', 'Admin/CategoryController')
+  .apiOnly()
+  .middleware(['is:master'])
+  
     Route
     .resource('accounts', 'Admin/AccountController')
     .apiOnly()
