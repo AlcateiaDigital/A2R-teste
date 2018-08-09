834f1d8cc5663c34ea1e4476c6ad9b0c76481337
AdhamWynston <adhamwynston@outlook.com>
2018-08-09 02:57:00 -0300

    Add validation end points

--- app/Controllers/Http/Seller/MenuOptionController.js
+++ app/Controllers/Http/Seller/MenuOptionController.js
@@ -2,10 +2,25 @@
 
 const MenuOption = use('App/Models/MenuOption')
 const Seller = use('App/Models/Seller')
+const { validate } = use('Validator')
 
 class MenuOptionController {
 
   async index ({ request, response, auth }) {
+
+    const data = request.all()
+
+    const rules = {
+      name: 'required|unique:menu_options|string',
+      priority: 'required|numeric'
+    }
+
+    const validation = await validate(data, rules);
+
+    if (validation.fails()) {
+      return response.status(422).send(validation._errorMessages)
+    }
+
 
     const seller = await Seller.findByOrFail('account_id', auth.user.account_id)
 
@@ -18,7 +33,7 @@ class MenuOptionController {
 
   async store ({ request, auth, response }) {
 
-    const data = request.only(["name", "priority", "slug"])
+    const data = request.only(["name", "priority"])
 
     const seller = await Seller.findByOrFail('account_id', auth.user.account_id)
 
@@ -45,7 +60,18 @@ class MenuOptionController {
 
   async update ({ params, request, auth,  response }) {
 
-    const data = request.only(["name", "priority", "slug"])
+    const data = request.all()
+
+    const rules = {
+      name: 'required|unique:menu_options|string',
+      priority: 'required|numeric'
+    }
+
+    const validation = await validate(data, rules);
+
+    if (validation.fails()) {
+      return response.status(422).send(validation._errorMessages)
+    }
 
     const userLogged = auth.user
 
