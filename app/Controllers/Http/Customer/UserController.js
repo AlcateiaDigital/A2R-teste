81bf449562674ccecb3d79542186c15e68268b3e
AdhamWynston <adhamwynston@outlook.com>
2018-08-29 14:36:56 -0300

    Update role user

--- app/Controllers/Http/Customer/UserController.js
+++ app/Controllers/Http/Customer/UserController.js
@@ -1,19 +1,20 @@
 'use strict'
 
 const User = use('App/Models/User')
-const Role = use('App/Models/Role')
+const Role = use('Adonis/Acl/Role')
 
 class UserController {
 
   async store ({ request, response }) {
 
     const data = request.only(["name", "email", "password", "phone_1"])
+
     const user = await User.create({...data, status: 'active'})
+    const role = await Role.query().where('slug', 'customer').firstOrFail()
+
+    await user.roles().attach([role.id])
+
     return user
-    const role = Role.firstOrFail('slug', 'customer')
-    return role
-    await user.roles().a
-    return
 
   }
 
