3a382609c93857f64917a4a05196a0438670e000
AdhamWynston <adhamwynston97@gmail.com>
2018-11-22 01:09:06 -0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -16,13 +16,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: {
-    host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
-    port: Env.get('DB_PORT', ''),
-    user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
-    password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
-    database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
-  },
+  connection: Env.get('DB_CONNECTION', 'sqlite'),
 
   /*
   |--------------------------------------------------------------------------
@@ -56,12 +50,13 @@ module.exports = {
   */
   mysql: {
     client: 'mysql',
-    connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+    connection: 
+      {
+        host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
+        port: Env.get('DB_PORT', ''),
+        user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
+        password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
+        database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
     },
     debug: Env.get('DB_DEBUG', false)
   },
